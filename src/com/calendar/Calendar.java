48af3436b7727d2166d22f8a00b57596ff6d40aa
Administrator <Administrator@Fang>
2018-08-30 11:26:22 +0900

    method 분리

--- src/com/calendar/Calendar.java
+++ src/com/calendar/Calendar.java
@@ -1,12 +1,38 @@
 package com.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
-	public static void main(String[] args) {
+
+	private static final int[] MaxDay = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public static int getMaxDay(int month) {
+			return MaxDay[month];
+	}
+	
+	public void printSampleCalendar() {
 		System.out.println("일    월    화    수    목    금    토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	public static void main(String[] args) {
+		
+
+		// 숫자 입력받아 해당 월의 최대 일수 출력
+		System.out.println("월을 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		
+		if(month>12 || month<1) {
+			System.out.println("범위를 벗어났습니다.");
+		}
+		else {
+			System.out.println(month + "월의 최대 일수는 " + getMaxDay(month) + "일입니다.");
+		}
+		sc.close();
 	}
 }
